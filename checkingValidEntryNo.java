0e4fb13cfac44c5b33766357a227cc0dd937f09f
Beginners1234 <himanshusingh4994@gmail.com>
2016-01-18 12:56:28 +0530

    Create checkingValidEntryNo.java

---
+++ checkingValidEntryNo.java
@@ -0,0 +1,11 @@
+public checkingValidEntryNo () {
+
+   String [] entries ;
+    public checkingValidEntryNo (String [] s) {
+         entries =s;
+         }
+     
+     public Boolean checkNo () {
+        if ((s[0].length()!=11)||(s[1].length()!=11)||(s[2].length()!=11)) return false;
+        else return true; }
+        
